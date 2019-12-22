fb89606714b7d22a8aa8c151598cdad7b0f5da79
rafi <muhammad.farook@bs-23.net>
2019-12-22 18:49:03 +0600

    3rd

--- app/src/main/java/com/example/daggertest2/di/AppComponent.kt
+++ app/src/main/java/com/example/daggertest2/di/AppComponent.kt
@@ -1,9 +1,11 @@
 package com.example.daggertest2.di
 
 import com.example.daggertest2.FirstFragment
+import com.example.daggertest2.di.module.CarModule
 import dagger.Component
 
-@Component
+@Component(modules = [CarModule::class])
+
 interface AppComponent {
 
    fun inject(target:FirstFragment)
--- app/src/main/java/com/example/daggertest2/di/module/CarModule.kt
+++ app/src/main/java/com/example/daggertest2/di/module/CarModule.kt
@@ -1,4 +1,11 @@
 package com.example.daggertest2.di.module
 
+import com.example.daggertest2.model.Car
+import dagger.Module
+import dagger.Provides
+
+@Module
 class CarModule {
+    @Provides
+    fun providesCar() : Car = Car()
 }
